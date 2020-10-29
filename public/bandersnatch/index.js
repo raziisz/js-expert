3cfd7531b21c8e0d51f48ec06dfbf0054f2a07e2
Luiz Felipe Carvalho Libertino <raziel_libertino@hotmail.com>
2020-10-29 16:02:13 +0000

    end day 4

--- public/bandersnatch/index.js
+++ public/bandersnatch/index.js
@@ -11,10 +11,13 @@ async function main() {
     const network = new Network({ host })
     const videoPlayer = new VideoMediaPlayer({
         manifestJSON,
-        network
+        network,
+        videoComponent
     })
     videoPlayer.initializeCodec();
     videoComponent.initializePlayer();
+
+    window.nextChunk = (data) => videoPlayer.nextChunk(data)
     
 }
 
--- public/bandersnatch/src/videoPlayer.js
+++ public/bandersnatch/src/videoPlayer.js
@@ -1,11 +1,15 @@
 class VideoMediaPlayer {
-  constructor({ manifestJSON, network }) {
+  constructor({ manifestJSON, network, videoComponent }) {
     this.manifestJSON = manifestJSON;
     this.network = network;
-    this.videoElement = null
-    this.sourceBuffer = null
-    this.selected = {}
+    this.videoComponent = videoComponent;
+
+    this.videoElement = null;
+    this.sourceBuffer = null;
+    this.activeItem = {};
+    this.selected = {};
     this.videoDuration = 0;
+    this.selections = [];
   }
 
   initializeCodec() {
@@ -36,13 +40,59 @@ class VideoMediaPlayer {
       //evitar rodar como "LIVE"
       mediaSource.duration = this.videoDuration;
       await this.fileDownload(selected.url)
+      setInterval(this.waitForQuestions.bind(this), 200);
     }
   }
 
+  waitForQuestions() {
+    const currentTime = parseInt(this.videoElement.currentTime);
+    const option = this.selected.at === currentTime;
+    if (!option) return;
+
+    //evita que o modal seja aberto 2x no mesmo segundo
+    if (this.activeItem.url === this.selected.url) return;
+    this.videoComponent.configureModal(this.selected.options);
+    this.activeItem = this.selected;
+  }
+  async currentFileResolution() {
+    const LOWEST_RESOLUTION = 144;
+    const prepareUrl = {
+      url: this.manifestJSON.finalizar.url,
+      fileResolution: LOWEST_RESOLUTION,
+      fileResolutionTag: this.manifestJSON.fileResolutionTag,
+      hostTag: this.manifestJSON.hostTag
+    }
+
+    const url = this.network.parseManifestURL(prepareUrl);
+    return this.network.getProperResolution(url);
+  }
+  async nextChunk(data) {
+    const key = data.toLowerCase();
+    const selected = this.manifestJSON[key];
+    this.selected = {
+      ...selected,
+      //ajustar o tempo que o modal vai aparecer, baseado no tempo corrente
+      at: parseInt(this.videoElement.currentTime + selected.at)
+    }
+    this.manageLag(this.selected);
+    //deixa o restante do video rodar enquanto baixa o novo
+    this.videoElement.play();
+    await this.fileDownload(selected.url);
+  }
+  manageLag(selected) {
+    if (!!~this.selections.indexOf(selected.url)) {
+      selected.at += 5
+      return;
+    }
+
+    this.selections.push(selected.url);
+  }
   async fileDownload(url) {
+    const fileResolution = await this.currentFileResolution();
+    console.log('currentResolution', fileResolution);
     const prepareUrl = {
       url,
-      fileResolution: 360,
+      fileResolution,
       fileResolutionTag: this.manifestJSON.fileResolutionTag,
       hostTag: this.manifestJSON.hostTag
     }
@@ -57,7 +107,7 @@ class VideoMediaPlayer {
   setVideoPlayerDuration(finalURL) {
     const bars = finalURL.split('/');
     const [name, videoDuration] = bars[bars.length - 1].split('-');
-    this.videoDuration += videoDuration;
+    this.videoDuration += parseFloat(videoDuration);
   }
 
   async processBufferSegments(allSegments) {
